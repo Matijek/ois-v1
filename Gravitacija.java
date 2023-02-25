37e7536815bb1a5bcd72a35ef62b3e550c4e9526
aleksgogic <ag41823@student.uni-lj.si>
2023-02-25 22:06:20 +0100

    popravljena za gravitacijo

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,4 @@
 import java.util.*;
-public class Gravitacija {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -9,8 +8,5 @@ public class Gravitacija {
         double r = 6.371 * Math.pow(10, 6);
         double v = sc.nextDouble();
         double a = c * m / ((r + v) * (r + v));
-        System.out.println(a);
-
+        
     }
-
-}
