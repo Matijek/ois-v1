c6fd0b1e99adb81d98047964115de00af65ac448
Matijek <zanjkovic.matija@gmail.com>
2023-02-25 22:59:31 +0100

    Začetna verzija

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,12 +1,7 @@
-import java.util.*;
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
 
-        double c = 6.674 * Math.pow(10, -11);
-        double m = 5.972 * Math.pow(10, 24);
-        double r = 6.371 * Math.pow(10, 6);
-        double v = sc.nextDouble();
-        double a = c * m / ((r + v) * (r + v));
-        
-    }
+public class Gravitacija {
+	public static void main(String[] args) {
+	
+	System.out.println("OIS je zakon!");
+	}
+}
