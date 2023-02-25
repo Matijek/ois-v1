f5cd9aecc7b70f068aaacc8c28a05e4f054dd95e
Matijek <zanjkovic.matija@gmail.com>
2023-02-25 23:25:16 +0100

    Odprava še ene napake

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,6 +5,8 @@ public class Gravitacija {
 	
 	System.out.println("OIS je zakon!");
 
+        Scanner sc = new Scanner(System.in);
+
         double c = 6.674 * Math.pow(10, -11);
         double m = 5.972 * Math.pow(10, 24);
         double r = 6.371 * Math.pow(10, 6);
